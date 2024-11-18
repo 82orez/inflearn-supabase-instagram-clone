a843a48cb70b461915d5049f5e8dd7ea2bf92c31
82orez <82orez@naver.com>
2024-11-18 19:08:15 +0900

    getUser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import { createClient } from "@/utils/supabase/server";
 export default async function Home() {
   const supabase = await createClient();
   const {
-    data: { session },
-  } = await supabase.auth.getSession();
+    data: { user },
+  } = await supabase.auth.getUser();
 
   return (
     <main className="flex flex-col justify-center items-center w-full">
       <div>Home</div>
-      <div>Welcome {session?.user.email} </div>
+      <div>Welcome {user?.user_metadata.name} 님!</div>
       <Logout />
     </main>
   );
--- src/components/main-layout-page/sidebar.tsx
+++ src/components/main-layout-page/sidebar.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use server";
 
 import HomeIcon from "@mui/icons-material/Home";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
@@ -6,10 +6,12 @@ import SearchIcon from "@mui/icons-material/Search";
 import SendIcon from "@mui/icons-material/Send";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import Link from "next/link";
-import { createClient } from "@/utils/supabase/client";
+import { createClient } from "@/utils/supabase/server";
+import { Button } from "@material-tailwind/react";
+import Logout from "@/components/auth-page/logout";
 
-export default function Sidebar() {
-  const supabase = createClient();
+export default async function Sidebar() {
+  const supabase = await createClient();
   return (
     <aside className={"border-4 h-screen w-1/3 max-w-[240px] min-w-[120px] p-6 flex flex-col justify-center items-center"}>
       <Link href="/goodjob">
@@ -25,14 +27,15 @@ export default function Sidebar() {
         <SendIcon className={"text-5xl text-blue-800"} />
         <div>메세지 보내기</div>
       </Link>
-      <div
-        onClick={() => {
-          supabase.auth.signOut();
-          return;
-        }}>
-        <ExitToAppIcon className={"text-5xl bg-amber-300"} />
-        <div>Log out</div>
-      </div>
+      {/*<div*/}
+      {/*  onClick={() => {*/}
+      {/*    supabase.auth.signOut();*/}
+      {/*    return;*/}
+      {/*  }}>*/}
+      {/*  <ExitToAppIcon className={"text-5xl bg-amber-300"} />*/}
+      {/*  <div>Log out</div>*/}
+      {/*</div>*/}
+      <Logout />
     </aside>
   );
 }
