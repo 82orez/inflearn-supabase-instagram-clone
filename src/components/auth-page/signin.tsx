d4f164b832cc95e7665437d5ea752cdf02381905
82orez <82orez@naver.com>
2024-11-18 17:01:36 +0900

    email auth-1

--- src/components/auth-page/signin.tsx
+++ src/components/auth-page/signin.tsx
@@ -24,6 +24,19 @@ export default function Signin({ setView }) {
     },
   });
 
+  const signInWithKakao = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "kakao",
+      options: {
+        redirectTo: process.env.NEXT_PUBLIC_VERCEL_URL
+          ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/auth/callback`
+          : "http://localhost:3000/api/auth/callback",
+      },
+    });
+    if (error) alert(error.message);
+    console.log(data);
+  };
+
   return (
     <div className={"flex flex-col justify-center items-center gap-10 h-screen bg-blue-gray-50"}>
       <div className="border-4 border-amber-600 w-full max-w-[480px] p-8 flex flex-col gap-3 bg-white">
@@ -55,6 +68,10 @@ export default function Signin({ setView }) {
           disabled={signInMutation.isPending}
           loading={signInMutation.isPending}>
           로그인 하기
+        </Button>
+        {/*@ts-ignore*/}
+        <Button color={"amber"} onClick={() => signInWithKakao()}>
+          카카오 로그인
         </Button>
       </div>
 
