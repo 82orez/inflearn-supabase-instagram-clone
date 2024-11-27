23892ecd00922678014ad1bb82c31e09a55b80b0
82orez <82orez@naver.com>
2024-11-28 05:56:40 +0900

    build modified-10

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,5 +1,5 @@
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
+// The client you created from the Server-Side Auth instructions
 import { createClient } from "@/utils/supabase/server";
 
 export async function GET(request: Request) {
@@ -12,10 +12,19 @@ export async function GET(request: Request) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
+      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+      const isLocalEnv = process.env.NODE_ENV === "development";
+      if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/kakao-error`);
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
